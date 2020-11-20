e6554136550e92003f8e1e7012a7390e02759a0b
jinsewoon <jinseun@gmail.com>
2020-11-20 13:18:00 +0900

    akademie
    스크립트 수정

--- js/akademie.js
+++ js/akademie.js
@@ -2,7 +2,9 @@ $(function(){
   $(".info_text").mouseover(function(){
         console.log($(this).parents("a").find(".hover_img").find("img"))
         $(this).parents("a").find(".hover_img").find("img").css("display","block");
+        $(this).parents("a").find(".info_text h2, .info_text div dl dt, .info_text div dl dd ").css("color","#ffffff");
       }).mouseout(function(){
         $(this).parents("a").find(".hover_img").find("img").css("display","none");
+        $(this).parents("a").find(".info_text h2, .info_text div dl dt, .info_text div dl dd ").css("color","#666666");
       });
 });
